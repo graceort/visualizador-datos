20e0642d2a62f69cae5a91524c3905dcee239099
graceort <grace.ortega@uees.edu.ec>
2025-10-06 10:25:29 -0500

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,14 @@
+import { ChartModule } from 'primeng/chart';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { HttpClientModule } from '@angular/common/http';
 
+@NgModule({
+  imports: [
+    ChartModule,
+    ButtonModule,
+    CardModule,
+    HttpClientModule,
+    // otros módulos...
+  ]
+})
