1823799f693f4730a02ce48e62af383040cad8af
graceort <grace.ortega@uees.edu.ec>
2025-10-06 10:24:37 -0500

    home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,1 +1,10 @@
-
+<p-card header="Visualizador de Datos">
+  <div class="grid">
+    <div class="col">
+      <button pButton label="Ver APIs" routerLink="/api-dashboard"></button>
+    </div>
+    <div class="col">
+      <button pButton label="Ver Gráficos" routerLink="/chart-view"></button>
+    </div>
+  </div>
+</p-card>
