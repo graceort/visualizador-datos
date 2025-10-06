57e6c7c29787fbd67782f0feafd43dd058a01774
graceort <grace.ortega@uees.edu.ec>
2025-10-06 10:27:17 -0500

    api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,1 +1,17 @@
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  getCovidStats() {
+    return this.http.get('https://api.covid19api.com/summary');
+  }
+
+  getCryptoPrices() {
+    return this.http.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd');
+  }
+
+  getWeather(city: string) {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=TU_API_KEY`);
+  }
+}
 
